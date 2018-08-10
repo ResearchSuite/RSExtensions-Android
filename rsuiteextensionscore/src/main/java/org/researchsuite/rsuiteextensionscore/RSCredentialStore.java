2a626089198fde5756f0022ce782e6dda587a36f
jdkizer9 <jdkizer9@users.noreply.github.com>
2018-08-10 14:02:09 -0400

    Moved RSCredential store to new ResearchSuite Extensions library

--- rsuiteextensionscore/src/main/java/org/researchsuite/rsuiteextensionscore/RSCredentialStore.java
+++ rsuiteextensionscore/src/main/java/org/researchsuite/rsuiteextensionscore/RSCredentialStore.java
@@ -6,9 +6,9 @@ import android.content.Context;
  * Created by jameskizer on 3/13/18.
  */
 
-public interface RSCredentialStore {
-    byte[] get(Context context, String key);
-    void set(Context context, String key, byte[] value);
-    void remove(Context context, String key);
-    boolean has(Context context, String key);
-}
+//public interface RSCredentialStore {
+//    byte[] get(Context context, String key);
+//    void set(Context context, String key, byte[] value);
+//    void remove(Context context, String key);
+//    boolean has(Context context, String key);
+//}
